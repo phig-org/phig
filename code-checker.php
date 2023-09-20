3b2c968a0e47b01878cfe271d2717badd1b91827
Jan Pecha <janpecha@email.cz>
2023-09-20 12:22:23 +0200

    code-checker: updated

--- code-checker.php
+++ code-checker.php
@@ -3,5 +3,5 @@
 return function (JP\CodeChecker\CheckerConfig $config) {
 	$config->setPhpVersion(new JP\CodeChecker\Version('5.6.0'));
 	$config->addPath('./src');
-	JP\CodeChecker\Tasks\AutoConfig::configure($config);
+	JP\CodeChecker\Sets\CzProjectMinimum::configure($config);
 };
